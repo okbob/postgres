27bd809565f263a4ede384c2efba2b671435faf2
okbob@github.com <pavel.stehule@gmail.com>
2022-07-18 16:45:26 +0200

    fix win build

--- src/tools/msvc/Mkvcbuild.pm
+++ src/tools/msvc/Mkvcbuild.pm
@@ -446,6 +446,8 @@ sub mkvcbuild
 	$pgdumpall->AddIncludeDir('src/backend');
 	$pgdumpall->AddFile('src/bin/pg_dump/pg_dumpall.c');
 	$pgdumpall->AddFile('src/bin/pg_dump/dumputils.c');
+	$pgdumpall->AddFile('src/bin/pg_dump/filter.c');
+	$pgdumpall->AddFile('src/bin/pg_dump/pg_backup_utils.c');
 	$pgdumpall->AddLibrary('ws2_32.lib');
 
 	my $pgrestore = AddSimpleFrontend('pg_dump', 1);
